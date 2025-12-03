2de9bfba4ffc3927b5f2be0ef250c654f845f44f
sarto-scuola <matteo.on.the.sk8@gmail.com>
2025-12-03 10:01:35 +0100

    index + css

--- index.php
+++ index.php
@@ -1,27 +1,45 @@
 <?php
-
+// PHP (se necessario per altre funzionalità)
 ?>
 
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
-<html>
+<html lang="it">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
+        <title>Sito di Canzoni</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="">
+        <link rel="stylesheet" href="style.css">
+        <!-- Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
     </head>
-    <body>
-        <!--[if lt IE 7]>
-            <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
-        <![endif]-->
-        <p>test git</p>
-        
-        <script src="" async defer></script>
+    <body style="background-color: black">
+        <div class="container-fluid">
+            <div class="row align-items-center justify-content-center">
+                <!-- Colonna sinistra (3/4) -->
+                <div class="col-lg-9 col-md-12 mb-4">
+                    <div class="underglow-box p-5" 
+                         style="height: 800px; display: flex; align-items: center; justify-content: center;">
+                        <div class="text-center">
+                            <h1 class="mb-4">Sezione sinistra (3/4)</h1>
+                            <p class="fs-5">Contenuto della sezione sinistra. Questa sezione occupa il 75% della larghezza della pagina su desktop.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <!-- Colonna destra (1/4) -->
+                <div class="col-lg-3 col-md-12 mb-4">
+                    <div class="underglow-box p-5" 
+                         style="height: 800px; display: flex; align-items: center; justify-content: center;">
+                        <div class="text-center">
+                            <h1 class="mb-4">Sezione destra (1/4)</h1>
+                            <p class="fs-5">Contenuto della sezione destra. Questa sezione occupa il 25% della larghezza della pagina su desktop.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </body>
 </html>
