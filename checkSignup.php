cafec9993c0910752efba9a4e6e76f14b4686004
5PAC3 <matteo.on.the.sk8@gmail.com>
2026-01-24 19:40:40 +0100

    Classe playlist manager + pwds + memo

--- checkSignup.php
+++ checkSignup.php
@@ -26,7 +26,7 @@ if($_POST["azione"]==="signup"){
     // Connessione al database
     $host = "localhost";
     $user = "root";
-    $db_password = "tiamotilda";
+    $db_password = "";
     $database = "clonefy";
     
     $conn = new mysqli($host, $user, $db_password, $database);
